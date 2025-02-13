5f1e7b3c4ba200c5a66f197cff4aa59a6a7ce9dd
Martina <martisantana565@gmail.com>
2025-02-13 01:53:00 -0300

    Edit useTitulo

--- src/hooks/useTitulo.jsx
+++ src/hooks/useTitulo.jsx
@@ -4,7 +4,7 @@ import { useEffect } from "react"
 const useTitulo = ( textoTitulo = 'Sin título') => {
 
     useEffect(() => {
-      document.title = `Educación IT - ${textoTitulo}`
+      document.title = `Rita Cosmeticos - ${textoTitulo}`
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     
